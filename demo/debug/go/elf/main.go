457218dbc50f573c61d35181cedeeb1bdbe6d9b1
fengpf <18221167541@163.com>
2018-12-20 10:54:28 +0800

    update lldb

---
+++ demo/debug/go/elf/main.go
@@ -0,0 +1,5 @@
+package main
+
+func main() {
+	// elf.R_INFO(uint32(targ.Dynid), uint32(elf.R_X86_64_64))
+}
---
+++ demo/runtime/goroutine/steal/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	ch := make(chan int)
+	done := make(chan struct{})
+
+	// runtime.GOMAXPROCS(1)
+	for i := 0; i < 3; i++ {
+		go func(idx int) {
+			// fmt.Println(idx, "go idx")
+			select {
+			case ch <- (idx + 1) * 2:
+				fmt.Println(idx, "Send result")
+			case <-done:
+				fmt.Println(idx, "Exiting")
+			}
+		}(i)
+	}
+
+	// statGroutine := func() {
+	// 	for {
+	// 		time.Sleep(time.Second)
+	// 		total := runtime.NumGoroutine()
+	// 		fmt.Printf("goroutine num(%d)\n", total)
+	// 	}
+	// }
+	// go statGroutine()
+
+	fmt.Println("Result: ", <-ch)
+	close(done)
+
+	time.Sleep(3 * time.Second)
+
+}
