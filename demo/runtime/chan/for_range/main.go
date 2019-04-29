07fd6ac6bd63ebbeed5b61427391cb88f0f2016a
fengpf <18221167541@163.com>
2019-04-29 23:45:44 +0800

    chan block lead to goroutine leak

--- demo/runtime/chan/for_range/main.go
+++ demo/runtime/chan/for_range/main.go
@@ -1,25 +1,21 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
-
 func main() {
-	ch := make(chan int, 10)
+	ch := make(chan int)
+	ch <- 1
 
-	var wg sync.WaitGroup
-	for i := 0; i < 10; i++ {
-		wg.Add(1)
-		go func(j int) {
-			ch <- j
-			wg.Done()
-		}(i)
-	}
-	wg.Wait()
-	close(ch)
+	// var wg sync.WaitGroup
+	// for i := 1; i < 1; i++ {
+	// 	wg.Add(1)
+	// 	go func(j int) {
+	// 		ch <- j
+	// 		wg.Done()
+	// 	}(i)
+	// }
+	// wg.Wait()
+	// close(ch)
 
-	for i := range ch {
-		fmt.Println("ch:", i)
-	}
+	// for i := range ch {
+	// 	fmt.Println("ch:", i)
+	// }
 }
