fbb282a7e4a463d7df4b25d1c82bc9b19b4d8d7f
fengpf <18221167541@163.com>
2019-04-30 20:36:09 +0800

    goroutine panic recover, concurrent map read&write

--- demo/runtime/chan/for_range/main.go
+++ demo/runtime/chan/for_range/main.go
@@ -2,7 +2,8 @@ package main
 
 func main() {
 	ch := make(chan int)
-	ch <- 1
+	// ch <- 1
+	<-ch
 
 	// ch := make(chan int, 1)
 
---
+++ demo/runtime/goroutine/panic/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"sync"
+)
+
+//G for goroutine
+type G struct {
+	wg  sync.WaitGroup
+	ctx context.Context
+}
+
+//Go for go func
+func (g *G) Go(f func()) {
+	g.wg.Add(1)
+	defer func() {
+		if err := recover(); err != nil {
+			println(err)
+		}
+		g.wg.Done()
+	}()
+	go f()
+}
+
+//Wait for go
+func (g *G) Wait() {
+	g.wg.Wait()
+}
+
+func main() {
+	g := new(G)
+	println(111)
+	// ctx, cancel := context.WithCancel(context.Background())
+	g.Go(func() {
+		panic("goroutine panic")
+	})
+
+	g.Go(func() {
+		println("!!11")
+	})
+
+	g.Wait()
+}
