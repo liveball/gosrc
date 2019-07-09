36f4cbd99136e8c423c5604dfd025ff47118d32c
fengpf <18221167541@163.com>
2019-07-09 19:13:57 +0800

    add atoi

---
+++ practice/base_learn/string/2int/main.go
@@ -0,0 +1,64 @@
+package main
+
+import "fmt"
+
+const (
+	maxUint = ^uint(0)
+	maxInt  = int(maxUint >> 1)
+)
+
+func main() {
+	println(^uint(0), maxUint>>1)
+
+	i, ok := atoi("-123")
+	fmt.Printf("i(%d), ok(%v)\n", i, ok)
+}
+
+// atoi parses an int from a string s.
+// The bool result reports whether s is a number
+// representable by a value of type int.
+func atoi(s string) (int, bool) {
+	if s == "" {
+		return 0, false
+	}
+
+	neg := false
+	if s[0] == '-' {
+		neg = true
+		s = s[1:]
+	}
+
+	un := uint(0)
+	for i := 0; i < len(s); i++ {
+		c := s[i]
+		if c < '0' || c > '9' {
+			return 0, false
+		}
+		if un > maxUint/10 {
+			// overflow
+			return 0, false
+		}
+		un *= 10
+		un1 := un + uint(c) - '0' //按字节计算
+		println(un, string(c), un1)
+		if un1 < un {
+			// overflow
+			return 0, false
+		}
+		un = un1
+	}
+
+	if !neg && un > uint(maxInt) {
+		return 0, false
+	}
+	if neg && un > uint(maxInt)+1 {
+		return 0, false
+	}
+
+	n := int(un)
+	if neg {
+		n = -n
+	}
+
+	return n, true
+}
